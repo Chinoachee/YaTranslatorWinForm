e87fc3ae493eb1a715b368a7c2e29b0e96d258f0
Chinoachee <152435959+Chinoachee@users.noreply.github.com>
2024-02-08 01:40:17 +0400

    Add textBox and ListBox in form

--- Window.cs
+++ Window.cs
@@ -5,9 +5,17 @@ namespace Translator {
         private Button _sourseButton;
         private Button _acceptButton;
         private Button _targetButton;
+
+        private TextBox _sourseTextBox;
+        private TextBox _targetTextBox;
+
+        private ListBox _sourseListBox;
+        private ListBox _targetListBox;
         public Window() {
             InitializationWindow();
             InitializationButton();
+            InitializationListBox();
+            InitializationTextBox();
         }
         private void InitializationWindow() {
             Size = new Size(_windowWidth, _windowHeight);
@@ -22,13 +30,42 @@ namespace Translator {
             Controls.Add(_acceptButton);
             Controls.Add(_targetButton);
         }
-
+        private void InitializationTextBox() {
+            _sourseTextBox = CreateTextBox(_sourseTextBoxWidth,_sourseTextBoxHeight,_sourseTextBoxXPosition,_sourseTextBoxYPosition,false);
+            _targetTextBox = CreateTextBox(_targetTextBoxWidth,_targetTextBoxHeight,_targetTextBoxXPosition - 1,_targetTextBoxYPosition,true);
+            Controls.Add(_sourseTextBox);
+            Controls.Add(_targetTextBox);
+        }
+        private void InitializationListBox() {
+            _sourseListBox = CreateListBox(_sourseListBoxWidth - 2,_sourseListBoxHeight,_sourseListBoxXPosition + 1,_sourseListBoxYPosition);
+            _targetListBox = CreateListBox(_targetListBoxWidth - 2,_targetListBoxHeight,_targetListBoxXPosition + 1,_targetListBoxYPosition);
+            Controls.Add(_sourseListBox);
+            Controls.Add(_targetListBox);
+        }
         private Button CreateButton(int width,int height,int Xposition,int Yposition) {
             return new Button() {
                 Width = width,
                 Height = height,
                 Location = new Point(Xposition,Yposition),
                 Visible = true,
+            };
+        }
+        private TextBox CreateTextBox(int width,int height,int Xposition,int Yposition,bool readOnly) {
+            return new TextBox() {
+                Width = width,
+                Height = height,
+                Location = new Point(Xposition,Yposition), 
+                Visible = true,
+                Multiline = true,
+                ReadOnly = readOnly,
+            };
+        }
+        private ListBox CreateListBox(int width,int height,int Xposition,int Yposition) {
+            return new ListBox() {
+                MaximumSize = new Size(width,height),
+                MinimumSize = new Size(width,20),
+                Location = new Point(Xposition,Yposition),
+                Visible = false,
             };
         }
     }
--- WindowPoles.cs
+++ WindowPoles.cs
@@ -20,5 +20,24 @@ namespace Translator {
         private const int _targetButtonXPosition = 335;
         private const int _targetButtonYPosition = 15;
 
+        private const int _sourseTextBoxWidth = 300;
+        private const int _sourseTextBoxHeight = 200;
+        private const int _sourseTextBoxXPosition = 0;
+        private const int _sourseTextBoxYPosition = 70;
+
+        private const int _targetTextBoxWidth = 300;
+        private const int _targetTextBoxHeight = 200;
+        private const int _targetTextBoxXPosition = 300;
+        private const int _targetTextBoxYPosition = 70;
+
+        private const int _sourseListBoxWidth = 250;
+        private const int _sourseListBoxHeight = 100;
+        private const int _sourseListBoxXPosition = 15;
+        private const int _sourseListBoxYPosition = 56;
+
+        private const int _targetListBoxWidth = 250;
+        private const int _targetListBoxHeight = 100;
+        private const int _targetListBoxXPosition = 335;
+        private const int _targetListBoxYPosition = 56;
     }
 }
