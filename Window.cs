f8acd55cecd2689ff7de6d650a7d9205d6ceba3f
Chinoachee <152435959+Chinoachee@users.noreply.github.com>
2024-02-08 00:04:48 +0400

    add Initialization window

--- Window.cs
+++ Window.cs
@@ -2,6 +2,16 @@
 using System.Drawing;
 namespace Translator {
     public class Window : Form {
-
+        private const int _windowWidth = 600;
+        private const int _windowHeight = 400;
+        public Window() {
+            InitializationWindow();
+        }
+        private void InitializationWindow() {
+            Width = _windowWidth; 
+            Height = _windowHeight;
+            MaximumSize = new Size(Width, Height);
+            MinimumSize = new Size(Width, Height);
+        }
     }
 }
