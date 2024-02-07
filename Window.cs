cb9f13dbac05f871b8efffae8fc2bc043413bbcd
Chinoachee <152435959+Chinoachee@users.noreply.github.com>
2024-02-08 02:14:45 +0400

    add realization button clicked

--- Window.cs
+++ Window.cs
@@ -1,6 +1,8 @@
 ﻿using System.Windows.Forms;
 using System.Drawing;
 using System;
+using System.Collections.Generic;
+using System.Linq;
 namespace Translator {
     public partial class Window : Form {
         private Button _sourseButton;
@@ -13,8 +15,21 @@ namespace Translator {
         private ListBox _sourseListBox;
         private ListBox _targetListBox;
 
+        Language lang = new Language();
 
         public Window() {
+            lang.AddSourseLanguage("Русский");
+            lang.AddSourseLanguage("Английский");
+            lang.AddSourseLanguage("Французский");
+            lang.AddSourseLanguage("Японский");
+
+            lang.AddTargetLanguage("Русский","Английский");
+            lang.AddTargetLanguage("Русский","Французский");
+            lang.AddTargetLanguage("Русский","Японский");
+
+            lang.AddTargetLanguage("Английский","Русский");
+            lang.AddTargetLanguage("Английский","Французский");
+            lang.AddTargetLanguage("Английский","Японский");
             InitializationWindow();
             InitializationButton();
             InitializationListBox();
@@ -29,6 +44,11 @@ namespace Translator {
             _sourseButton = CreateButton(_sourseButtonWidth, _sourseButtonHeight,_sourseButtonXPosition,_sourseButtonYPosition);
             _acceptButton = CreateButton(_acceptButtonWidth,_acceptButtonHeight,_acceptButtonXPosition,_acceptButtonYPosition);
             _targetButton = CreateButton(_targetButtonWidth,_targetButtonHeight,_targetButtonXPosition,_targetButtonYPosition);
+
+            _sourseButton.Click += SourseButton_Clicked;
+
+            _targetButton.Click += TargetButton_Clicked;
+
             Controls.Add(_sourseButton);
             Controls.Add(_acceptButton);
             Controls.Add(_targetButton);
@@ -42,10 +62,42 @@ namespace Translator {
         private void InitializationListBox() {
             _sourseListBox = CreateListBox(_sourseListBoxWidth - 2,_sourseListBoxHeight,_sourseListBoxXPosition + 1,_sourseListBoxYPosition);
             _targetListBox = CreateListBox(_targetListBoxWidth - 2,_targetListBoxHeight,_targetListBoxXPosition + 1,_targetListBoxYPosition);
+
+            _sourseListBox.Click += SourseListBox_Clicked;
+
             Controls.Add(_sourseListBox);
             Controls.Add(_targetListBox);
         }
 
+        private void SourseButton_Clicked(object sender, EventArgs e) {
+            _sourseListBox.Items.Clear();
+            _sourseListBox.Items.AddRange(lang.GetSourseLanguages().ToArray());
+            _sourseListBox.Items.Add("Добавить новый язык");
+            _sourseListBox.Height = _sourseListBox.Items.Count * 15;
+            _sourseListBox.Visible = SwitchListBox(_sourseListBox);
+            _targetListBox.Visible = false;
+        }
+        private void TargetButton_Clicked(object sender,EventArgs e) {
+            if(!string.IsNullOrEmpty(_sourseButton.Text)) {
+                _targetListBox.Items.Clear();
+                _targetListBox.Items.AddRange(lang.GetTargetLanguages(_sourseButton.Text).ToArray());
+                _targetListBox.Items.Add("Добавить новый язык");
+                _targetListBox.Height = _targetListBox.Items.Count * 15;
+                _targetListBox.Visible = SwitchListBox(_targetListBox);
+                _sourseListBox.Visible = false;
+            }
+        }
+
+        private void SourseListBox_Clicked(object sender,EventArgs e) {
+            if(_sourseListBox.SelectedItems.Count != _sourseListBox.Items.Count) {
+                _sourseButton.Text = _sourseListBox.SelectedItem.ToString();
+            }
+            _sourseListBox.Visible = SwitchListBox(_sourseListBox);
+        }
+
+        private bool SwitchListBox(ListBox listBox) {
+            return !listBox.Visible;
+        }
         private Button CreateButton(int width,int height,int Xposition,int Yposition) {
             return new Button() {
                 Width = width,
