e814ef3b4a925cedb2a830058155931efd8d7cb1
Chinoachee <152435959+Chinoachee@users.noreply.github.com>
2024-02-08 02:32:35 +0400

    add class Word

---
+++ Word.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Translator {
+    public class Word {
+        private Dictionary<string,HashSet<string>> _words;
+        public Word() {
+            _words = new Dictionary<string,HashSet<string>>();
+        }
+        public void AddSourseWord(string sourseWord) {
+            if(!string.IsNullOrEmpty(sourseWord) && !_words.ContainsKey(sourseWord)) {
+                _words.Add(sourseWord,new HashSet<string>());
+            }
+        }
+        public void AddTargetWord(string sourseWord,string targetWord) {
+            if(!string.IsNullOrEmpty(targetWord)) {
+                _words[sourseWord].Add(targetWord);
+            }
+        }
+        public string GetTargetWord(string sourseWord) {
+            return _words[sourseWord].First();
+        }
+    }
+}
