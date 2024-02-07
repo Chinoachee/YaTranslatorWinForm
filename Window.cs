ad85c9eb66e310f8bd315c03bb218ff981541822
Chinoachee <152435959+Chinoachee@users.noreply.github.com>
2024-02-08 00:54:56 +0400

    Add button in form

--- Window.cs
+++ Window.cs
@@ -2,18 +2,34 @@
 using System.Drawing;
 namespace Translator {
     public partial class Window : Form {
-        private const int _windowWidth = 600;
-        private const int _windowHeight = 400;
-        
+        private Button _sourseButton;
+        private Button _acceptButton;
+        private Button _targetButton;
         public Window() {
             InitializationWindow();
+            InitializationButton();
         }
         private void InitializationWindow() {
-            Width = _windowWidth; 
-            Height = _windowHeight;
+            Size = new Size(_windowWidth, _windowHeight);
             MaximumSize = new Size(Width, Height);
             MinimumSize = new Size(Width, Height);
         }
-        
+        private Button CreateButton(int width,int height,int Xposition, int Yposition) {
+            return new Button() {
+                Width = width,
+                Height = height,
+                Location = new Point(Xposition, Yposition),
+                Visible = true,
+            };
+        }
+
+        private void InitializationButton() {
+            _sourseButton = CreateButton(_sourseButtonWidth, _sourseButtonHeight,_sourseButtonXPosition,_sourseButtonYPosition);
+            _acceptButton = CreateButton(_acceptButtonWidth,_acceptButtonHeight,_acceptButtonXPosition,_acceptButtonYPosition);
+            _targetButton = CreateButton(_targetButtonWidth,_targetButtonHeight,_targetButtonXPosition,_targetButtonYPosition);
+            Controls.Add(_sourseButton);
+            Controls.Add(_acceptButton);
+            Controls.Add(_targetButton);
+        }
     }
 }
--- WindowPoles.cs
+++ WindowPoles.cs
@@ -1,17 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
+﻿using System.Windows.Forms;
 
 namespace Translator {
     public partial class Window : Form {
-        private Button _sourseButton;
-        private Button _acceptButton;
-        private Button _targetButton;
-        private void InitializationButton() {
+        private const int _windowWidth = 600;
+        private const int _windowHeight = 400;
 
-        }
+        private const int _sourseButtonWidth = 250;
+        private const int _sourseButtonHeight = 40;
+        private const int _sourseButtonXPosition = 15;
+        private const int _sourseButtonYPosition = 15;
+
+        private const int _acceptButtonWidth = 40;
+        private const int _acceptButtonHeight = 40;
+        private const int _acceptButtonXPosition = 280;
+        private const int _acceptButtonYPosition = 15;
+
+        private const int _targetButtonWidth = 250;
+        private const int _targetButtonHeight = 40;
+        private const int _targetButtonXPosition = 335;
+        private const int _targetButtonYPosition = 15;
+
     }
 }
